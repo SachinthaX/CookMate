d7a4836ac72e21a3aa4e9f959d7d1e3815ad293b
ChamathYukthila <ychamath913@gmail.com>
2025-05-06 21:38:14 +0530

    fix error

--- cookMate/src/main/java/com/paf/cookMate/BackendApplication.java
+++ cookMate/src/main/java/com/paf/cookMate/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.cookmate.backend;
+package com.paf.cookMate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- cookMate/src/test/java/com/paf/cookMate/BackendApplicationTests.java
+++ cookMate/src/test/java/com/paf/cookMate/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cookmate.backend;
+package com.paf.cookMate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
