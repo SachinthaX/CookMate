76a4885862fe239179c632dd1fc56f63be21a945
Abheetha Dhananjaya <148703787+abheethasma@users.noreply.github.com>
2025-05-07 22:05:26 +0530

    backend initial commit

--- backend/src/main/java/com/cookmate/backend/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cookmate.backend.repository;
-
-import com.cookmate.backend.model.Post;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PostRepository extends MongoRepository<Post, String> {
-}
