bc144aaa1a0a9721856dad291ef62a90e826ebd0
Nipuna Sachintha <nipunasachintha2001@gmail.com>
2025-05-13 23:53:43 +0530

    Add User model and MongoDB repositories  - Created User.java with profile fields - Added UserRepository.java for user data access - Created UserSession.java and UserSessionRepository.java for session tracking

---
+++ cookMate/src/main/java/com/paf/cookMate/Model/User.java
@@ -0,0 +1,37 @@
+package com.paf.cookMate.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "users")
+public class User {
+    
+    @Id
+    private String id;
+    
+    private String name;
+    
+    @Indexed(unique = true)
+    private String email;
+    
+    private String password;
+    
+    private String profilePicture;
+    
+    private String bio;
+    
+    private LocalDateTime createdAt;
+    
+    private LocalDateTime updatedAt;
+    
+    private boolean active = true;
+} 
